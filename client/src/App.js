ed92dfa7ddb68f1e733f5696fd7aeff3527155da
bazina anastasia <abazina858@gmail.com>
2024-03-02 14:31:43 +0400

    client init 2

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App d-flex justife-">
       <img src={ex} style={{height:600,width:600}}/>
-      <button style={{height:100,width:100}}>Мяу</button>
+      <button style={{height:200,width:400}}>Мяу</button>
     </div>
   );
 }
